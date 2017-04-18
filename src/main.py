003be5ca44c20c64fcff80843a95a958c8d2b7f7
Ali Kaafarani <ali@kvikshaug.no>
2017-04-18 16:30:56 +0200

    feat: send metrics to statsd

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import socket
 import os
 import psycopg2
 import threading
@@ -12,9 +13,9 @@ def main():
 
 def report_active_connections():
     connection = psycopg2.connect("host=%s dbname=%s user=%s password=%s" % (
-        os.environ['HOST'],
-        os.environ['DATABASE'],
-        os.environ['USER'],
+        os.environ['DB_HOST'],
+        os.environ['DB_DATABASE'],
+        os.environ['DB_USER'],
         'password',  # TODO
     ))
     cursor = connection.cursor()
@@ -26,7 +27,11 @@ def report_active_connections():
         else:
             states[row[0]] += 1
 
-    # TODO: Report to librato
+    statsd_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    for key, value in states.items():
+        name = 'postgres.active_connections.%s' % key.lower().replace(' ', '_')
+        message = ("%s:1|c" % (name)).encode('utf-8')
+        statsd_socket.sendto(message, (os.environ["STATSD_HOST"], 8125))
 
 
 if __name__ == '__main__':
