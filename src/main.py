9f5e1a770a29042c18a598332e813ad73db1e307
Ali Kaafarani <ali@kvikshaug.no>
2017-04-18 14:40:18 +0200

    feat: execute each query in a thread

--- src/main.py
+++ src/main.py
@@ -1,4 +1,15 @@
+import threading
+import time
+
+
 def main():
+    while True:
+        threading.Thread(target=report_active_connections).start()
+        time.sleep(30)  # Report every 30 seconds
+
+
+def report_active_connections():
+    # TODO
     pass
 
 
